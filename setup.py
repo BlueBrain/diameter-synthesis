bdb1145f4cda483d18c90da8dc45cc50ce35b672
Adrien Berchet <adrien.berchet@epfl.ch>
2021-04-30 10:40:43 +0200

    Install missing JSON files
    Change-Id: I5642e9d55e4de621805bff67757b9bfe18e1fc63

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
     },
     python_requires=">=3.6",
     packages=find_packages(exclude="tests"),
+    include_package_data=True,
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Programming Language :: Python",
