5363beeb06d3b482190127bd41691cec5768202b
Adrien Berchet <adrien.berchet@epfl.ch>
2024-11-06 11:30:01 +0100

    Build: Drop Py38 support

--- setup.py
+++ setup.py
@@ -34,11 +34,11 @@ test_reqs = [
     "diff_pdf_visually>=1.7.0",
     "decorator>=4",
     "mock>=3",
-    "pytest>=6",
-    "pytest-click>=1",
-    "pytest-console-scripts>=1.3",
-    "pytest-cov>=3",
-    "pytest-html>=2",
+    "pytest>=6.1",
+    "pytest-click>=1.1",
+    "pytest-console-scripts>=1.4",
+    "pytest-cov>=4.1",
+    "pytest-html>=3.2",
     "pytest-xdist>=2",
 ]
 
@@ -55,7 +55,7 @@ setup(
     },
     license="Apache License 2.0",
     packages=find_namespace_packages(include=["diameter_synthesis*"]),
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     use_scm_version=True,
     setup_requires=[
         "setuptools_scm",
@@ -78,10 +78,10 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
 )
